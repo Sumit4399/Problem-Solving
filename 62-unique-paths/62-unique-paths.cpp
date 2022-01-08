8a77dfabab2b53502e658d38a37e245888f7ec77
Sumit Anand <60431204+Sumit4399@users.noreply.github.com>
2022-01-08 22:06:55 +0530

    Time: 5 ms (9.35%), Space: 6 MB (68.03%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -2,20 +2,31 @@ class Solution {
 public:
     
     
-    int uniquePaths(int m, int n) {
+//     int uniquePaths(int m, int n) {
         
-       int dp[101][101];
-       for(int i = 0;i < n;i++){
-           dp[m - 1][i] = 1;
-       }
-       for(int i = 0;i < m;i++){
-           dp[i][n - 1] = 1;
-       }
-       for(int i = m - 2;i >= 0;i--){
-           for(int j = n - 2;j >= 0;j--){
-               dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
-           }
-       }
-       return dp[0][0];
+//        int dp[101][101];
+//        for(int i = 0;i < n;i++){
+//            dp[m - 1][i] = 1;
+//        }
+//        for(int i = 0;i < m;i++){
+//            dp[i][n - 1] = 1;
+//        }
+//        for(int i = m - 2;i >= 0;i--){
+//            for(int j = n - 2;j >= 0;j--){
+//                dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
+//            }
+//        }
+//        return dp[0][0];
+//     }
+    
+   // **Combinatorics**
+        int uniquePaths(int m,int n){
+        int  N = m + n - 2;
+        int r = m - 1;
+        double ans = 1;
+        for(int i = 1;i <= r;i++){
+            ans = (ans * (N - r + i))/i;
+        }
+        return int(ans);
     }
 };
