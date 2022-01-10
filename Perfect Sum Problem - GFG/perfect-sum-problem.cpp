20009b3c449ad12053828bb88312f3f6588e37f7
Sumit Anand <60431204+Sumit4399@users.noreply.github.com>
2022-01-10 22:56:33 +0530

    Added solution - LeetHub

--- Perfect Sum Problem - GFG/perfect-sum-problem.cpp
+++ Perfect Sum Problem - GFG/perfect-sum-problem.cpp
@@ -10,31 +10,29 @@ class Solution{
 	{
         // Your code goes here
         int mod= 1000000007;
-       int t[n+1][sum+1];
-       for(int i=0;i<n+1;i++)
-       {
-           for(int j=0;j<sum+1;j++)
-           {
-               if(i==0)
-               {
-                   if(j==0)
+      int t[n+1][sum+1];
+      for(int i=0;i<n+1;i++)
+      {
+          for(int j=0;j<sum+1;j++)
+          {
+              if(i==0)
+              {
+                  if(j==0)
                      t[i][j] = 1;
-                   else
+                  else
                      t[i][j] = 0;
-               }
-               else if(arr[i-1]<=j)
-               {
-                   t[i][j] = (t[i-1][j-arr[i-1]]%mod + t[i-1][j]%mod)%mod;
-               }
-               else
-               {
-                   t[i][j] = t[i-1][j]%mod;
-               }
-           }
-       }
-       return (int)t[n][sum]%mod;
-	
-	  
+              }
+              else if(arr[i-1]<=j)
+              {
+                  t[i][j] = (t[i-1][j-arr[i-1]]%mod + t[i-1][j]%mod)%mod;
+              }
+              else
+              {
+                  t[i][j] = t[i-1][j]%mod;
+              }
+          }
+      }
+      return (int)t[n][sum]%mod;
 	}
 	  
 };
