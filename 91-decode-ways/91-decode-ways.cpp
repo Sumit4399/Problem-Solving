dc4f49bbe367dcaaf03c18a4d74882ef124f1c46
Sumit Anand <60431204+Sumit4399@users.noreply.github.com>
2022-01-19 13:26:48 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (69.95%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,51 +1,8 @@
 class Solution {
 public:
-   // int numDecodings(string s) {
+    int numDecodings(string s) {
         
-//         int n= s.length();
-//          if(n==1){
-//             if(s[0]=='0')
-//                 return 0;
-//         }
-        
-//         vector<int> dp(n,0);
-//         dp[0]=1;
-        
-//         if(n>=2){
-//             if(s[0]=='0')
-//                 return 0;
-//         }
-        
-//         for(int i=1; i<n; i++)
-//         {
-//             if(s[i-1]=='0' && s[i]=='0')
-//                 dp[i]=0;
-            
-//             else if(s[i-1]=='0' && s[i]!=0)
-//                 dp[i]=dp[i-1];
-            
-//             else if(s[i-1]!='0' && s[i]=='0')
-//             {
-//                 if(s[i-1]=='1' || s[i-1]=='2')
-//                     dp[i]= (i>=2 ? dp[i-2] : 1);
-//                 else
-//                     dp[i]=0;
-//             }
-            
-//             else
-//             {
-//                 if(stoi(s.substr(i-1, i+1)) <=26)
-//                     dp[i]= dp[i-1] + (i>=2 ? dp[i-2] : 1);
-//                 else
-//                     dp[i]= dp[i-1];
-//             }  
-//         }
-        
-//         return dp[n-1];
-        
-        
-         int numDecodings(string s) {
-        if(s.length()==1){
+         if(s.length()==1){
             if(s[0]=='0')
                 return 0;
         }
@@ -83,6 +40,6 @@ public:
         }
     }
         return dp[s.length()-1];
-
-    }
+        
+ }
 };
