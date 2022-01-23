bb5fbd8c5e866fe4feb3408b71adcca0bfd69533
Sumit Anand <60431204+Sumit4399@users.noreply.github.com>
2022-01-23 16:56:33 +0530

    Time: 86 ms (44.61%), Space: 23.5 MB (99.40%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
@@ -24,24 +24,3 @@ public:
     }
 };
 
-
-
-
-// int rows = matrix.size();
-//         int cols = rows>0 ? matrix[0].size() :0;
-//         int dp[rows+1][cols+1];
-//         memset(dp,0,sizeof(dp));
-//         //vector<vector<int>> dp(rows+1,vector<int>(cols+1,0));
-//         int maxlen=0;
-//         for (int i = 1; i <= rows; i++)
-//         {
-//              for (int j = 1; j <= cols; j++)
-//              {
-//                  if(matrix[i-1][j-1]=='1')
-//                  {
-//                      dp[i][j]= min({dp[i-1][j-1], dp[i][j-1], dp[i-1][j]})+1;
-//                      maxlen= max(maxlen, dp[i][j]);
-//                  }
-//              }
-//         }
-//         return maxlen*maxlen;
