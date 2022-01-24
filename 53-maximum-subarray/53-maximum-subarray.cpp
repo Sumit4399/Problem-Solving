b706d9e2501f475504cef424d7af3894485fd31c
Sumit Anand <60431204+Sumit4399@users.noreply.github.com>
2022-01-24 16:14:28 +0530

    Time: 165 ms (29.94%), Space: 67.8 MB (11.75%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
+    int maxSubArray(vector<int>& arr) {
         
-        int curr_sum=0;// for storing sum of current looping elements
-        int res=INT_MIN;// stores max value after every iteration
+//         int curr_sum=0;// for storing sum of current looping elements
+//         int res=INT_MIN;// stores max value after every iteration
         
-        for(int i=0; i<nums.size(); i++)
+//         for(int i=0; i<nums.size(); i++)
+//         {
+//             curr_sum+= nums[i];
+//             res= max(res, curr_sum);
+//             if(curr_sum<0)
+//             {
+//                 curr_sum=0;
+//             }
+//         }
+//         return res;
+        
+        
+        //if all the elements in array are negtive
+        int maxi= arr[0];
+        int curr= arr[0];
+        
+        for(int i=1; i<size(arr); i++)
         {
-            curr_sum+= nums[i];
-            res= max(res, curr_sum);
-            if(curr_sum<0)
-            {
-                curr_sum=0;
-            }
+            curr= max(arr[i], curr+arr[i]);
+            maxi= max(maxi, curr);
         }
-        return res;
-    }
+        
+        return maxi;
+        }
 };
