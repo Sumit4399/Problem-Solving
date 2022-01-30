ee45ade64419fa4a91721a53ada49217abbcb252
Sumit Anand <60431204+Sumit4399@users.noreply.github.com>
2022-01-31 00:14:36 +0530

    Time: 3 ms (62.15%), Space: 5.9 MB (24.92%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -1,7 +1,23 @@
 class Solution {
 public:
-    int mySqrt(int x) {
+//     int mySqrt(int x) {
         
-        return sqrt(x);
+//         return sqrt(x);
+//     }
+    
+    int mySqrt(int x){
+        
+        long long s=0,e=INT_MAX,ans=0;
+        while(s<=e){
+            long long m=s+(e-s)/2;
+            
+            if(m*m<=x){
+                ans=m;
+                s=m+1;
+            }
+            else e=m-1;
+        }
+      
+        return ans;
     }
 };
