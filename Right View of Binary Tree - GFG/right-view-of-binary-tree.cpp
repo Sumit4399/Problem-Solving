8ad3975f9d1da5d792d90f44f0c5d220276e579e
Sumit Anand <60431204+Sumit4399@users.noreply.github.com>
2022-02-02 16:31:19 +0530

    Added solution - LeetHub

--- Right View of Binary Tree - GFG/right-view-of-binary-tree.cpp
+++ Right View of Binary Tree - GFG/right-view-of-binary-tree.cpp
@@ -40,34 +40,51 @@ struct Node
 class Solution
 {
     public:
+    
+    void helper(Node* root,int level,vector<int>&ans)
+   {
+       if(root==NULL) return;
+       if(ans.size()==level) ans.push_back(root->data);
+       helper(root->right,level+1,ans);
+       helper(root->left,level+1,ans);
+   }
+   //Function to return list containing elements of right view of binary tree.
+   vector<int> rightView(Node *root)
+   {
+      vector<int>ans;
+      helper(root,0,ans);
+      return ans;
+   }
+    
+    
     //Function to return list containing elements of right view of binary tree.
-    vector<int> rightView(Node *root)
-    {
-       vector<int> res;
-       if(root==NULL)
-       return res;
-       queue<Node*> q;
-       q.push(root);
+    // vector<int> rightView(Node *root)
+    // {
+    //   vector<int> res;
+    //   if(root==NULL)
+    //   return res;
+    //   queue<Node*> q;
+    //   q.push(root);
        
-       while(!q.empty())
-       {
-           int n= q.size();
-           for(int i=0; i<n; i++)
-           {
-               Node*curr= q.front();
-               q.pop();
-               if(i==0)
-               {
-                   res.push_back(curr->data);
-               }
-               if(curr->right!=NULL)
-               q.push(curr->right);
-               if(curr->left!=NULL)
-               q.push(curr->left);
-           }
-       }
-       return res;
-    }
+    //   while(!q.empty())
+    //   {
+    //       int n= q.size();
+    //       for(int i=0; i<n; i++)
+    //       {
+    //           Node*curr= q.front();
+    //           q.pop();
+    //           if(i==0)
+    //           {
+    //               res.push_back(curr->data);
+    //           }
+    //           if(curr->right!=NULL)
+    //           q.push(curr->right);
+    //           if(curr->left!=NULL)
+    //           q.push(curr->left);
+    //       }
+    //   }
+    //   return res;
+    // }
 };
 
 
