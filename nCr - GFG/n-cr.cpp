053ad587ab6cbcea4bbce7966c30c0f8ba49e915
Sumit Anand <60431204+Sumit4399@users.noreply.github.com>
2022-02-06 21:58:29 +0530

    Added solution - LeetHub

--- nCr - GFG/n-cr.cpp
+++ nCr - GFG/n-cr.cpp
@@ -10,6 +10,37 @@ using namespace std;
 class Solution{
 public:
     
+    // int mod= 1e9+7;
+    // int nCr(int n, int r){
+        
+    //     int t[n+1][r+1];
+    //     for(int i=0; i<n+1; i++)
+    //     {
+    //         for(int j=0; j<r+1; j++)
+    //         {
+    //             if(i==j || j==0)
+    //             t[i][j]=1;
+                
+    //             else if(i<j)
+    //             t[i][j]=0;
+    //         }
+    //     }
+        
+    //     //standard formula fpr binomial coefficient
+    //     //C(n, k) = C(n-1, k-1) + C(n-1, k)
+    //     //C(n, 0) = C(n, n) = 1
+    //     for(int i=1; i<n+1; i++)
+    //     {
+    //         for(int j=1; j<r+1; j++)
+    //         {
+    //             t[i][j]= (t[i-1][j-1]%mod + t[i-1][j]%mod)%mod;
+    //         }
+    //     }
+        
+    //     return t[n][r]%mod;
+    // }
+    
+    
     int mod= 1e9+7;
     int nCr(int n, int r){
         
@@ -26,7 +57,7 @@ public:
             }
         }
         
-        //satndard formula fpr binomial coefficient
+        //standard formula fpr binomial coefficient
         //C(n, k) = C(n-1, k-1) + C(n-1, k)
         //C(n, 0) = C(n, n) = 1
         for(int i=1; i<n+1; i++)
