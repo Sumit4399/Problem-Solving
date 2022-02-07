4e7f4ca8a68e0eb23d541c35fd0dbe0313d0c641
Sumit Anand <60431204+Sumit4399@users.noreply.github.com>
2022-02-07 12:13:05 +0530

    Time: 7 ms (24.08%), Space: 6.7 MB (67.00%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -2,14 +2,26 @@ class Solution {
 public:
     char findTheDifference(string s, string t) {
         
-        int s1= 0;
-        int s2= t[s.length()];
-        for(int i=0; i<s.length(); i++)
-        {
-            s1+= s[i];
-            s2+= t[i];
-        }
+//         int s1= 0;
+//         int s2= t[s.length()];
+//         for(int i=0; i<s.length(); i++)
+//         {
+//             s1+= s[i];
+//             s2+= t[i];
+//         }
         
-        return char(abs(s1-s2));
+//         return char(abs(s1-s2));
+//     }
+        
+		sort(s.begin(), s.end());
+		sort(t.begin(), t.end());
+		int i = 0;
+		while(i < s.size()){
+			if(s[i] != t[i]){
+				return t[i];
+			}
+			i++;
+		}
+		return t[s.size()];
     }
 };
