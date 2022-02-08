2d18023926c506fcd16974d05ae2f2edbfb140bd
Sumit Anand <60431204+Sumit4399@users.noreply.github.com>
2022-02-08 08:29:40 +0530

    Time: 4 ms (43.34%), Space: 5.7 MB (95.57%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -3,17 +3,17 @@ public:
     int addDigits(int num) {
         
         int sum=0;
-        while(num>0){
-            sum+= num%10;
+        while(num>9){
+            sum= num%10;
             num= num/10;
-            
-            if(num==0 && sum>9)
-            {
-                num=sum;
-                sum=0;
-            }
+            num+=sum;
+            // if(num==0 && sum>9)
+            // {
+            //     num=sum;
+            //     sum=0;
+            // }
         }
         
-        return sum;
+        return num;
     }
 };
