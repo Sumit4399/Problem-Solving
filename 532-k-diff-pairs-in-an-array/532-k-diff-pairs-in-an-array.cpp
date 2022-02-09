c8552aca8b0f4d5438f1196ada3c5b642e0b9db0
Sumit Anand <60431204+Sumit4399@users.noreply.github.com>
2022-02-09 12:33:56 +0530

    Time: 26 ms (51.47%), Space: 13.3 MB (37.26%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
@@ -2,20 +2,41 @@ class Solution {
 public:
     int findPairs(vector<int>& nums, int k) {
         
-        sort(nums.begin(), nums.end());
-        set<vector<int>> s;
-        int count=0;
-        for(int i=0; i<size(nums)-1; i++)
-        {
-             for(int j=i+1; j<size(nums); j++)
-             {
-                 if(abs(nums[i]-nums[j])==k)
-                 {
-                     s.insert({nums[i], nums[j]});
-                 }
-             }
+    //     sort(nums.begin(), nums.end());
+    //     set<vector<int>> s;
+    //     int count=0;
+    //     for(int i=0; i<size(nums)-1; i++)
+    //     {
+    //          for(int j=i+1; j<size(nums); j++)
+    //          {
+    //              if(abs(nums[i]-nums[j])==k)
+    //              {
+    //                  s.insert({nums[i], nums[j]});
+    //              }
+    //          }
+    //     }
+    //     count=s.size();
+    //     return count;
+    // }
+        
+        
+        unordered_map<int, int > freq;
+        for(auto num : nums) {
+            freq[num]++;
         }
-        count=s.size();
+        int count = 0;
+        for(auto it: freq) {
+            if(k == 0) {
+                if(it.second > 1) {
+                    count++;
+                }
+            } else {
+                if(freq.find(it.first + k) != freq.end()) {
+                    count++;
+                }
+            }
+        }
         return count;
     }
+        
 };
