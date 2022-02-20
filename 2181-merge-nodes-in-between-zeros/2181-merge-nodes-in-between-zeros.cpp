13707addff756cbb0cf06c4dc7a796547c90ef6e
Sumit Anand <60431204+Sumit4399@users.noreply.github.com>
2022-02-21 00:14:30 +0530

    Time: 660 ms (89.47%), Space: 274.6 MB (5.26%) - LeetHub

---
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cpp
@@ -0,0 +1,37 @@
+/**
+
+Today we have drink vodka and we mean Atishay patni,Sushant verma,Avinash Kumar and our dearest friend Sumit Anand.We have meet after          years and we are happy that such a great moment we have got to share but we are missing ours rest of our T.Y.I.C member that they are not with us.we are greatful we have got such a moment to share our feelings after a great time.At last i would like to rest my words...........
+            Avinash Kumar 2K19,Chemical Engineering
+            
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeNodes(ListNode* head) {
+        
+        ListNode * dummy=new ListNode(-1);
+        ListNode * temp=dummy;
+        head=head->next;
+        int sum=0;
+        while(head)
+        {
+            if(head->val!=0)
+                sum+=head->val;
+            else
+            {
+                temp->next=new ListNode(sum);
+                temp=temp->next;
+                sum=0;
+            }
+            head=head->next;
+        }
+        return dummy->next;
+    }  
+};
