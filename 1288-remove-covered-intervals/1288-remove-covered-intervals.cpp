1982436d43ecef7c590346464b7727253580022c
Sumit Anand <60431204+Sumit4399@users.noreply.github.com>
2022-02-20 16:50:20 +0530

    Time: 20 ms (95.61%), Space: 11.3 MB (89.47%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -3,22 +3,8 @@ public:
     
  
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
-        
-//         int count=0;
-        
-//         vector<int>v;
-//         sort(intervals.begin(), intervals.end());
-//         for(int i=0; i<size(intervals); i++)
-//         {
-//             v.push_back(intervals[i].first);
-//         }
-        
-// //         int r=intervals.size();
-// //         int c=intervals[0].size();
-        
-//         sortcol(intervals.size(), intervals[0].size());
-        
-        int count=0; 
+          
+        int count= intervals.size();
         int n=intervals.size();
         for(int i=0;i<n;i++) 
         {
@@ -29,15 +15,16 @@ public:
             {
                 if(i==j||intervals[j][0]==-1) 
                     continue;
-                else if((intervals[i][0]<=intervals[j][0])&&(intervals[j][1]<=intervals[i][1]))
+                else if((intervals[i][0]<=intervals[j][0])
+                        &&(intervals[j][1]<=intervals[i][1]))
                 {
                     intervals[j][0]=-1;
-                    count++;
+                    count--;
                 }
             }
         }
         
-        int left_uncovered=n-count; 
-        return left_uncovered;
+        //int left_uncovered=n-count; 
+        return count;
     }
 };
