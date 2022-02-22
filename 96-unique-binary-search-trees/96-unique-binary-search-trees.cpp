b36575d0e0625c6db9dc1b459f25e6534a9289df
Sumit Anand <60431204+Sumit4399@users.noreply.github.com>
2022-02-22 16:18:23 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (89.01%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -3,6 +3,15 @@ public:
 
     int numTrees(int n) {
        
+        //recursive
+//         int result=0;
+//         if(n==1 || n==0)
+//             return 1;
+//         for(int i=0;i<n;i++)
+//             result+=numTrees(i)*numTrees(n-i-1);
+//         return result;
+          
+        
         int dp[20];
         memset(dp,0, sizeof(dp));
         
@@ -18,5 +27,6 @@ public:
         }
         
         return dp[n];
+        
     }
 };
