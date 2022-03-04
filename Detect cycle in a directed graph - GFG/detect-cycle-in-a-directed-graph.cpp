507a2fda6866552a84ee05a7f21ac2dee2ef787c
Sumit Anand <60431204+Sumit4399@users.noreply.github.com>
2022-03-05 00:28:06 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
@@ -6,36 +6,82 @@ using namespace std;
 class Solution {
   public:
   
-    bool dfs(int V, vector<int> adj[], vector<bool> &visited, vector<bool> &recstack, int i){
-        visited[i] = true;
-        recstack[i] = true;
-        for(auto &e : adj[i]){
-            if(visited[e] == false && dfs(V, adj, visited, recstack, e)){
-                return true;
-            }
-            else if(recstack[e] == true){
-                return true;
-            }
-        }
-        recstack[i] = false;
-        return false;
-    }
+    // bool dfs(int V, vector<int> adj[], vector<bool> &visited, vector<bool> &recstack, int i){
+    //     visited[i] = true;
+    //     recstack[i] = true;
+    //     for(auto &e : adj[i]){
+    //         if(visited[e] == false && dfs(V, adj, visited, recstack, e)){
+    //             return true;
+    //         }
+    //         else if(recstack[e] == true){
+    //             return true;
+    //         }
+    //     }
+    //     recstack[i] = false;
+    //     return false;
+    // }
     
     
-    bool isCyclic(int V, vector<int> adj[]) {
-        vector<bool> visited(V, false);
-        vector<bool> recstack(V, false);
-        bool isCyclic = false;
-        for(int i = 0; i < V; i++){
-            if(visited[i] == false){
-                isCyclic = dfs(V, adj, visited, recstack, i);
-            }
-            if(isCyclic){
-                return true;
-            }
-        }
-        return false;
-    }
+    // bool isCyclic(int V, vector<int> adj[]) {
+    //     vector<bool> visited(V, false);
+    //     vector<bool> recstack(V, false);
+    //     bool isCyclic = false;
+    //     for(int i = 0; i < V; i++){
+    //         if(visited[i] == false){
+    //             isCyclic = dfs(V, adj, visited, recstack, i);
+    //         }
+    //         if(isCyclic){
+    //             return true;
+    //         }
+    //     }
+    //     return false;
+    // }
+    
+    
+   //Cycle Detection in Directed Graph using BFS(Kahn's Algorithm)
+   bool isCyclic(int V, vector<int> adj[]){
+       
+       queue<int> q;
+ 	    //find indegree of each node
+ 	    vector<int> indegree(V,0);
+ 	    for(int i=0; i<V; i++)
+ 	    {
+ 	        for(auto it : adj[i])
+ 	        {
+ 	            indegree[it]++;
+ 	        }
+ 	    }
+ 	    
+ 	    //push the node to queue if any node has 0 indegree
+ 	    for(int i=0; i<V; i++)
+ 	    {
+ 	        if(indegree[i]==0)
+ 	        {
+ 	            q.push(i);
+ 	        }
+ 	    }
+ 	    
+ 	    int res=0;
+ 	    while(!q.empty())
+ 	    {
+ 	        int node=q.front();
+ 	        q.pop();
+ 	        res++;
+ 	        //find indegree of curr node pushed into the queue
+ 	        for(auto it : adj[node])
+ 	        {
+ 	            indegree[it]--;
+ 	            //if found zero push it into queue
+ 	            if(indegree[it]==0)
+ 	            {
+ 	                q.push(it);
+ 	            }
+ 	        }
+ 	    }
+ 	    
+        return (res==V? false : true);
+   }
+// }
 };
 
 // { Driver Code Starts.
