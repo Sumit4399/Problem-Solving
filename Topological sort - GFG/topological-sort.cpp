e1921407f5b7e94be216e37cf265bc9c698f7c47
Sumit Anand <60431204+Sumit4399@users.noreply.github.com>
2022-03-04 23:44:45 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.cpp
+++ Topological sort - GFG/topological-sort.cpp
@@ -6,43 +6,89 @@ using namespace std;
 class Solution
 {
 	public:
+	//DFS approach
+// 	void topo(int node, vector<bool>& vis, stack<int>&st, vector<int> adj[]){
+	    
+// 	    vis[node]= true;
+// 	    for(auto it : adj[node])
+// 	    {
+// 	        if(!vis[it])
+// 	        {
+// 	            topo(it, vis, st, adj);
+// 	        }
+// 	    }
+// 	    st.push(node);
+// 	}
 	
-	void topo(int node, vector<bool>& vis, stack<int>&st, vector<int> adj[]){
+// 	//Function to return list containing vertices in Topological order. 
+// 	vector<int> topoSort(int V, vector<int> adj[]) 
+// 	{
+// 	    stack<int> st;
+// 	    vector<bool> vis(V,false);
 	    
-	    vis[node]= true;
-	    for(auto it : adj[node])
-	    {
-	        if(!vis[it])
-	        {
-	            topo(it, vis, st, adj);
-	        }
-	    }
-	    st.push(node);
-	}
-	
-	//Function to return list containing vertices in Topological order. 
-	vector<int> topoSort(int V, vector<int> adj[]) 
-	{
-	    stack<int> st;
-	    //int n= V.size();
-	    vector<bool> vis(V,false);
+// 	    for(int i=0; i<V; i++)
+// 	    {
+// 	        if(vis[i]==false)
+// 	        {
+// 	            topo(i, vis, st, adj);
+// 	        }
+// 	    }
 	    
-	    for(int i=0; i<V; i++)
-	    {
-	        if(vis[i]==false)
-	        {
-	            topo(i, vis, st, adj);
-	        }
-	    }
-	    
-	    vector<int> res;
-	    while(!st.empty())
-	    {
-	        res.push_back(st.top());
-	        st.pop();
-	    }
-	    return res;
-	}
+// 	    vector<int> res;
+// 	    while(!st.empty())
+// 	    {
+// 	        res.push_back(st.top());
+// 	        st.pop();
+// 	    }
+// 	    return res;
+// 	}
+
+
+
+//Function to return list containing vertices in Topological order. 
+//BFS APPROACH (KAHN'S ALGO)
+ 	vector<int> topoSort(int V, vector<int> adj[]) {
+ 	    
+ 	    queue<int> q;
+ 	    //find indegree of each node
+ 	    vector<int> indegree(V,0);
+ 	    for(int i=0; i<V; i++)
+ 	    {
+ 	        for(auto it : adj[i])
+ 	        {
+ 	            indegree[it]++;
+ 	        }
+ 	    }
+ 	    
+ 	    //push the node to queue if any node has 0 indegree
+ 	    for(int i=0; i<V; i++)
+ 	    {
+ 	        if(indegree[i]==0)
+ 	        {
+ 	            q.push(i);
+ 	        }
+ 	    }
+ 	    
+ 	    vector<int> res;
+ 	    while(!q.empty())
+ 	    {
+ 	        int node=q.front();
+ 	        q.pop();
+ 	        res.push_back(node);
+ 	        //find indegree of curr node pushed into the queue
+ 	        for(auto it : adj[node])
+ 	        {
+ 	            indegree[it]--;
+ 	            //if found zero push it into queue
+ 	            if(indegree[it]==0)
+ 	            {
+ 	                q.push(it);
+ 	            }
+ 	        }
+ 	    }
+ 	    return res;
+ 	}
+
 };
 
 // { Driver Code Starts.
