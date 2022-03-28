22dc0d56ae7b0d282fa628e25699e1413808e4f5
Sumit Anand <60431204+Sumit4399@users.noreply.github.com>
2022-03-29 00:48:22 +0530

    Time: 69 ms (73.28%), Space: 42.3 MB (97.35%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,35 +1,47 @@
 class Solution {
 public:
-    int maxScore(vector<int>& card, int k) {
+    
+//    int maxScore(vector<int>& card, int k) {
         
-        vector<int> prefix;
-        int sum=0;
-        int n=card.size();
+//         vector<int> prefix;
+//         int sum=0;
+//         int n=card.size();
         
-        for(int i=0; i<n; i++)
-        {
-            sum+=card[i];
-            prefix.push_back(sum);
-        }
+//         for(int i=0; i<n; i++)
+//         {
+//             sum+=card[i];
+//             prefix.push_back(sum);
+//         }
         
-        int res=0;
-        int temp;
-        for(int i=0;i<=k;i++) {
-            if(i==0) {
-                if(prefix.size()-k-1==-1) {
-                    temp = prefix.back();
-                }
-                else{
-                    temp = prefix.back()-prefix[prefix.size()-k-1];
-                }
-            }
-            else {
-                temp = prefix[i-1] + prefix.back()-prefix[prefix.size()-1-(k-i)];
-            }
-            if(temp>res) {
-                res = temp;
-            }
-        }
-        return res;
+//         int res=0;
+//         int temp;
+//         for(int i=0;i<=k;i++) {
+//             if(i==0) {
+//                 if(prefix.size()-k-1==-1) {
+//                     temp = prefix.back();
+//                 }
+//                 else{
+//                     temp = prefix.back()-prefix[prefix.size()-k-1];
+//                 }
+//             }
+//             else {
+//                 temp = prefix[i-1] + prefix.back()-prefix[prefix.size()-1-(k-i)];
+//             }
+//             if(temp>res) {
+//                 res = temp;
+//             }
+//         }
+//         return res;       
+//    }
+    
+    
+    
+    int maxScore(vector<int>& C, int K) {
+        int total = 0;
+        for (int i = 0; i < K; i++) total += C[i];
+        int best = total;
+        for (int i = K - 1, j = C.size() - 1; i>=0;  i--, j--)
+            total += C[j] - C[i], best = max(best, total);
+        return best;
     }
 };
