83cc07f62e2cb6cc0e651db39e2c09b2f2bc21c2
Sumit Anand <60431204+Sumit4399@users.noreply.github.com>
2022-03-29 10:22:03 +0530

    Time: 14 ms (94.54%), Space: 18 MB (20.60%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -2,23 +2,33 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         
-        int res=INT_MIN;
-        unordered_set<int> s;
+//         int res=INT_MIN;
+//         unordered_set<int> s;
+        
+//         for(int i=0; i<nums.size(); i++)
+//         {
+//             s.insert(nums[i]);
+//         }
+        
+//         for(int i=0; i<nums.size(); i++)
+//         {
+//             if(s.find(i) == s.end())
+//                 res= i;
+//         }
+        
+//         if(res==INT_MIN)
+//             return nums.size();
+//         else
+//             return res;
+        
+        int n= nums.size();
+        int tot= n*(n+1)/2;
+        int sum=0;
         
         for(int i=0; i<nums.size(); i++)
         {
-            s.insert(nums[i]);
+            sum+=(nums[i]);
         }
-        
-        for(int i=0; i<nums.size(); i++)
-        {
-            if(s.find(i) == s.end())
-                res= i;
-        }
-        
-        if(res==INT_MIN)
-            return nums.size();
-        else
-            return res;
+        return tot-sum;
     }
 };
