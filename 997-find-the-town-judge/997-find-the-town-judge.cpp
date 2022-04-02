00b38f4544f81905f6ec66e7ccd832066293c12d
Sumit Anand <60431204+Sumit4399@users.noreply.github.com>
2022-04-02 19:46:02 +0530

    Time: 263 ms (38.14%), Space: 62.7 MB (48.66%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.cpp
+++ 997-find-the-town-judge/997-find-the-town-judge.cpp
@@ -2,16 +2,38 @@ class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
         
-        vector<int> dis(n+1, 0);
-        for(auto it : trust)
+//         if(n==1)
+//             return 1;
+//         vector<int> dis(n+1, 0);
+//         for(auto it : trust)
+//         {
+//             dis[it[0]]--;
+//             dis[it[1]]++;
+//         }
+        
+//         for(int i=1; i<=n; i++)
+//         {
+//             if(dis[i]==n-1)
+//                 return i;
+//         }
+//         return -1;
+        
+        
+        //using hashmap
+        if(n==1)
+            return 1;
+        map<int, int> out;
+        map<int, int> in;
+        
+        for(int i=0; i<trust.size(); i++)
         {
-            dis[it[0]]--;
-            dis[it[1]]++;
+           out[trust[i][0]]++;
+            in[trust[i][1]]++;
         }
         
         for(int i=1; i<=n; i++)
         {
-            if(dis[i]==n-1)
+            if(out[i]==0 && in[i]==n-1)
                 return i;
         }
         
