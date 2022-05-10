f0c7defc1701538e0100eeb334123847eb2bcfcd
Sumit Anand <60431204+Sumit4399@users.noreply.github.com>
2022-05-10 21:40:25 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (68.75%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,25 +1,48 @@
 class Solution {
 public:
    
-        vector<vector<int>> subsets(vector<int>& nums) {
+      //iterative
+//         vector<vector<int>> subsets(vector<int>& nums) {
             
-        vector<vector<int>> ans;
-        int n = nums.size();
+//         vector<vector<int>> ans;
+//         int n = nums.size();
         
-        ans.push_back({});
+//         ans.push_back({});
         
-        for(int i=0; i<n; i++)
+//         for(int i=0; i<n; i++)
+//         {
+//             int sz = ans.size();
+//             for(int j=0; j<sz; j++)
+//             {
+//                 vector<int> temp = ans[j];
+//                 temp.push_back(nums[i]);
+//                 ans.push_back(temp);
+//             }
+//         }
+//         return ans;
+//     }
+    
+    
+        //recursive(back-tracking)
+    void solve(int start, vector<int>& nums, vector<int> &curr, 
+               vector<vector<int>> &res){
+    
+        res.push_back(curr);
+        
+        for(int i=start; i<nums.size(); i++)
         {
-            int sz = ans.size();
-            for(int j=0; j<sz; j++)
-            {
-                vector<int> temp = ans[j];
-                temp.push_back(nums[i]);
-                ans.push_back(temp);
-            }
+            curr.push_back(nums[i]);
+            solve(i+1, nums, curr, res);
+            curr.pop_back(); //back_track
         }
-        
-        return ans;
- 
     }
+    
+        vector<vector<int>> subsets(vector<int>& nums){
+            
+            vector<vector<int>> res;
+            vector<int> curr;   
+
+            solve(0, nums, curr, res);
+            return res;
+        }
 };
