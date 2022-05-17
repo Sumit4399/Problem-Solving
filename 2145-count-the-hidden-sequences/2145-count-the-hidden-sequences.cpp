61f96c3ce2ccd4807f85cedcb998d9f73a813f6d
Sumit Anand <60431204+Sumit4399@users.noreply.github.com>
2022-05-17 16:44:54 +0530

    Time: 277 ms (24.35%), Space: 106.6 MB (82.61%) - LeetHub

--- 2145-count-the-hidden-sequences/2145-count-the-hidden-sequences.cpp
+++ 2145-count-the-hidden-sequences/2145-count-the-hidden-sequences.cpp
@@ -4,23 +4,32 @@ public:
     int numberOfArrays(vector<int>& differences, int lower, int upper) {
         
         
-        lld valley = 0;
-        lld peek = 0;
-        lld offset = 0;
+//         lld valley = 0;
+//         lld peek = 0;
+//         lld offset = 0;
         
-        for (int v : differences) {
-            offset += v;
-            if (offset > peek) {
-                peek = offset;
-            }
-            if (offset < valley) {
-                valley = offset;
-            }
+//         for (int v : differences) {
+//             offset += v;
+//             if (offset > peek) {
+//                 peek = offset;
+//             }
+//             if (offset < valley) {
+//                 valley = offset;
+//             }
+//         }
+//         int cnt = (int)((upper - peek) - (lower - valley) + 1);
+//         if (cnt <= 0) {
+//             return 0; 
+//         }
+//         return cnt;
+        
+        
+        long a = 0, ma = 0, mi = 0;
+        for (int d: differences) {
+            a += d;
+            ma = max(ma, a);
+            mi = min(mi, a);
         }
-        int cnt = (int)((upper - peek) - (lower - valley) + 1);
-        if (cnt <= 0) {
-            return 0; 
-        }
-        return cnt;
+        return max(0L, (upper - lower) - (ma - mi) + 1);
     }
 };
