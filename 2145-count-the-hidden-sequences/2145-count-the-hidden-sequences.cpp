4053c4a23c69b4bacb798e4c3defd4d7d41a9ed0
Sumit Anand <60431204+Sumit4399@users.noreply.github.com>
2022-05-17 16:41:11 +0530

    Time: 246 ms (38.84%), Space: 106.6 MB (82.61%) - LeetHub

---
+++ 2145-count-the-hidden-sequences/2145-count-the-hidden-sequences.cpp
@@ -0,0 +1,26 @@
+typedef long long int lld;
+class Solution {
+public:
+    int numberOfArrays(vector<int>& differences, int lower, int upper) {
+        
+        
+        lld valley = 0;
+        lld peek = 0;
+        lld offset = 0;
+        
+        for (int v : differences) {
+            offset += v;
+            if (offset > peek) {
+                peek = offset;
+            }
+            if (offset < valley) {
+                valley = offset;
+            }
+        }
+        int cnt = (int)((upper - peek) - (lower - valley) + 1);
+        if (cnt <= 0) {
+            return 0; 
+        }
+        return cnt;
+    }
+};
