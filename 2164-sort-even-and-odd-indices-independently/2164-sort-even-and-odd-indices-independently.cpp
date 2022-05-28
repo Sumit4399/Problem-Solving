74683d73388826b448d53714d1d2a5ba01df604b
Sumit Anand <60431204+Sumit4399@users.noreply.github.com>
2022-05-28 11:15:22 +0530

    Time: 11 ms (50.37%), Space: 12.4 MB (11.85%) - LeetHub

--- 2164-sort-even-and-odd-indices-independently/2164-sort-even-and-odd-indices-independently.cpp
+++ 2164-sort-even-and-odd-indices-independently/2164-sort-even-and-odd-indices-independently.cpp
@@ -2,63 +2,63 @@ class Solution {
 public:
     vector<int> sortEvenOdd(vector<int>& nums) {
        
-//         vector<int> res;
-//         priority_queue<int> odd;
-//         priority_queue<int, vector<int>, greater<int>> even;
+        vector<int> res;
+        priority_queue<int> odd;
+        priority_queue<int, vector<int>, greater<int>> even;
         
-//        for(int i=0; i<nums.size(); i++)
-//         {
-//             if(nums[i]%2==0)
-//                 even.push(nums[i]);
-//             else
-//                 odd.push(nums[i]);
-//         }
-        
-//         for(int i=0; i<nums.size(); i++)
-//         {
-//             if(nums[i]%2==0)
-//             {
-//                 res.push_back(even.top());
-//                 even.pop();
-//             }
-//             else
-//             {
-//                 res.push_back(odd.top());
-//                 odd.pop();
-//             }
-//         }
-//         return res;
-        
-        
-        vector<int> odd, even;
-        for(int i = 0; i < nums.size(); i++) 
+       for(int i=0; i<nums.size(); i++)
         {
-            if(i%2==1)
+            if(i%2==0)
+                even.push(nums[i]);
+            else
+                odd.push(nums[i]);
+        }
+        
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(i%2==0)
             {
-                odd.push_back(nums[i]);
-            } 
+                res.push_back(even.top());
+                even.pop();
+            }
             else
             {
-                even.push_back(nums[i]);
+                res.push_back(odd.top());
+                odd.pop();
             }
         }
+        return res;
         
-        sort(odd.begin(), odd.end(), greater<int>());
-        sort(even.begin(), even.end());
         
-        for(int i = 0; i < nums.size(); i++) 
-        {
-            if(i%2==1)
-            {
-                nums[i] = odd[0];
-                odd.erase(odd.begin());
-            } 
-            else 
-            {
-                nums[i] = even[0];
-                even.erase(even.begin());
-            }
-        }
-        return nums;
+//         vector<int> odd, even;
+//         for(int i = 0; i < nums.size(); i++) 
+//         {
+//             if(i%2==1)
+//             {
+//                 odd.push_back(nums[i]);
+//             } 
+//             else
+//             {
+//                 even.push_back(nums[i]);
+//             }
+//         }
+        
+//         sort(odd.begin(), odd.end(), greater<int>());
+//         sort(even.begin(), even.end());
+        
+//         for(int i = 0; i < nums.size(); i++) 
+//         {
+//             if(i%2==1)
+//             {
+//                 nums[i] = odd[0];
+//                 odd.erase(odd.begin());
+//             } 
+//             else 
+//             {
+//                 nums[i] = even[0];
+//                 even.erase(even.begin());
+//             }
+//         }
+//         return nums;
     }
 };
