7c7a1faca031260a4d7e45e9a35b89ab9dd5b932
Sumit Anand <60431204+Sumit4399@users.noreply.github.com>
2022-05-30 00:33:53 +0530

    Time: 432 ms (25.00%), Space: 71.4 MB (50.00%) - LeetHub

--- 2284-sender-with-largest-word-count/2284-sender-with-largest-word-count.cpp
+++ 2284-sender-with-largest-word-count/2284-sender-with-largest-word-count.cpp
@@ -1,71 +1,70 @@
 class Solution {
 public:
     
-//     int words(string &s)
-//     {
-//         int c=0;
-//         for(char c : s)
-//         {
-//             if(c== ' ')
-//                 c++;
-//         }
-//         return c+1;
-//     }
-
-//     string largestWordCount(vector<string>& mes, vector<string>& sen) {
-        
-//         map<string, int> mp;
-//         for(int i=0; i<mes.size(); i++)
-//         {
-//             int co= words(mes[i]);
-//             mp[sen[i]]+= co;
-//         }
-        
-//         int c=0;
-//         string res="";
-        
-//         //iterating on map
-//         for(auto it= mp.begin(); it!=mp.end(); it++)
-//         {
-//             if(it->second >= c)
-//             {
-//                 c= it->second;
-//                 res= it->first;
-//             }
-//         }
-//         return res;
-//     }
-    
-    
-    int countWords(string &str){
-        
-        int count = 0;
-        for(char ch:str)
+    int words(string &s)
+    {
+        int cn=0;
+        for(char c : s)
         {
-            if(ch == ' ') ++count;
+            if(c == ' ')
+                cn++;
         }
-        return count+1;
+        return cn+1;
     }
-    
-    string largestWordCount(vector<string>& messages, vector<string>& senders){
+
+    string largestWordCount(vector<string>& mes, vector<string>& sen) {
         
         map<string, int> mp;
-        for(int i = 0; i<messages.size(); ++i)
+        for(int i=0; i<mes.size(); i++)
         {
-            int count = countWords(messages[i]);
-            mp[senders[i]]+=count;
+            int co= words(mes[i]);
+            mp[sen[i]]+= co;
         }
+        int c=0;
+        string res="";
         
-        string ans = "";
-        int count = 0;
-        for(auto it = mp.begin(); it!=mp.end(); ++it)
+        //iterating on map
+        for(auto it= mp.begin(); it!=mp.end(); it++)
         {
-            if(it->second >= count)
+            if(it->second >= c)
             {
-                count = it->second;
-                ans = it->first;
+                c= it->second;
+                res= it->first;
             }
         }
-        return ans;
+        return res;
     }
+    
+    
+//     int countWords(string &str){
+        
+//         int count = 0;
+//         for(char ch:str)
+//         {
+//             if(ch == ' ') ++count;
+//         }
+//         return count+1;
+//     }
+    
+//     string largestWordCount(vector<string>& messages, vector<string>& senders){
+        
+//         map<string, int> mp;
+//         for(int i = 0; i<messages.size(); ++i)
+//         {
+//             int count = countWords(messages[i]);
+//             mp[senders[i]]+=count;
+//         }
+        
+//         string ans = "";
+//         int count = 0;
+//         for(auto it = mp.begin(); it!=mp.end(); ++it)
+//         {
+//             if(it->second >= count)
+//             {
+//                 count = it->second;
+//                 ans = it->first;
+//             }
+//         }
+//         return ans;
+//     }
 };
