dd29465314ef307d62f297b092b306c5e42c9ce8
Sumit Anand <60431204+Sumit4399@users.noreply.github.com>
2022-06-06 21:38:04 +0530

    Time: 76 ms (32.66%), Space: 14.6 MB (31.40%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -11,25 +11,60 @@ public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         
         
+//         if(headA==NULL || headB==NULL)
+//             return NULL;
+//        //using SET
+//        unordered_set<ListNode*> s;
+//         ListNode * t= headA;
+//           while(t!=NULL)
+//           {
+//               s.insert(t);
+//               t= t->next;
+//           }
+
+//         t= headB;
+//         while(t!=NULL)
+//         {
+//             if(s.find(t) != s.end())
+//                 return t;
+            
+//             t= t->next;
+//         }
+//           return NULL;  
+     
+        
+        
+        //efficient approach
         if(headA==NULL || headB==NULL)
             return NULL;
-       //using SET
-       unordered_set<ListNode*> s;
-        ListNode * t= headA;
-      while(t!=NULL)
-      {
-          s.insert(t);
-          t= t->next;
-      }
-		
-        t= headB;
-        while(t!=NULL)
+        
+        ListNode * l1= headA;
+        ListNode * l2= headB;
+        
+        while(l1!=NULL && l2!=NULL)
         {
-            if(s.find(t) != s.end())
-                return t;
+            if(l1==l2)
+                return l1;
             
-            t= t->next;
+            else if(l1->next==NULL && l2!=NULL)
+            {
+                l1=headB;
+                l2= l2->next;
+            }
+            
+            else if(l2->next==NULL && l1!=NULL)
+            {
+                l2=headA;
+                l1= l1->next;
+            }
+            
+            else
+            {
+                l1= l1->next;
+                l2= l2->next;
+            }
         }
-          return NULL;  
-    }
+        
+        return NULL;
+    }      
 };
