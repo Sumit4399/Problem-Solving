ecbe55e60014867b08bc8f5700a56c07e3542806
Sumit Anand <60431204+Sumit4399@users.noreply.github.com>
2022-06-08 12:06:28 +0530

    Time: 26 ms (36.32%), Space: 9.1 MB (37.39%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -2,29 +2,60 @@ class Solution {
 public:
     
     //memoised code
-    int memo(int i, int j, string &s1, string &s2, vector<vector<int>> &dp){
-        if(j==0)
-            return i;
-        if(i==0)
-            return j;
-       if(dp[i][j] !=-1)
-           return dp[i][j];
+//     int memo(int i, int j, string &s1, string &s2, vector<vector<int>> &dp){
+//         if(j==0)
+//             return i;
+//         if(i==0)
+//             return j;
+//        if(dp[i][j] !=-1)
+//            return dp[i][j];
 
-        if(s1[i-1]==s2[j-1])
-            return dp[i][j]= 0 + memo(i-1, j-1, s1, s2, dp);
+//         if(s1[i-1]==s2[j-1])
+//             return dp[i][j]= 0 + memo(i-1, j-1, s1, s2, dp);
 
-        return dp[i][j]=1+ min(memo(i, j-1, s1, s2, dp), 
-                               min(memo(i-1, j, s1, s2, dp), memo(i-1, j-1, s1, s2, dp)));
+//         return dp[i][j]=1+ min(memo(i, j-1, s1, s2, dp), 
+//                                min(memo(i-1, j, s1, s2, dp), memo(i-1, j-1, s1, s2, dp)));
 
-    //     int in= 1+ memo(i, j-1, s1, s2); //insert
-    //     int del= 1+ memo(i-1, j, s1, s2); //delete
-    //     int rep= 1+ memo(i-1, j-1, s1, s2); //replace
-    }
+//     //     int in= 1+ memo(i, j-1, s1, s2); //insert
+//     //     int del= 1+ memo(i-1, j, s1, s2); //delete
+//     //     int rep= 1+ memo(i-1, j-1, s1, s2); //replace
+//     }
 
-    int minDistance(string s1, string s2) {
+//     int minDistance(string s1, string s2) {
+//         int n= s1.size();
+//         int m= s2.size();
+//         vector<vector<int>> dp(n+1, vector<int> (m+1,-1));
+//         return memo(n, m, s1, s2, dp);
+//     }
+    
+    
+    //tabulation form
+    int minDistance(string s1, string s2)
+    {
         int n= s1.size();
         int m= s2.size();
         vector<vector<int>> dp(n+1, vector<int> (m+1,-1));
-        return memo(n, m, s1, s2, dp);
+
+        for(int i=0; i<=n; i++)
+        {
+            dp[i][0]= i;
+        }
+        for(int j=0; j<=m; j++)
+        {
+            dp[0][j]= j;
+        }
+
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=m; j++)
+            {
+                if(s1[i-1]==s2[j-1])
+                    dp[i][j]= 0 + dp[i-1][j-1];
+                else
+                    dp[i][j]=1+ min(dp[i][j-1], min(dp[i-1][j], dp[i-1][j-1]));
+            }
+        }
+        return dp[n][m];
     }
+    
 };
