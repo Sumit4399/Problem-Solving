e4a7a7e80b3beef57e5d394b0acd9e209b2f2c5b
Sumit Anand <60431204+Sumit4399@users.noreply.github.com>
2022-06-09 01:04:22 +0530

    Time: 8 ms (85.45%), Space: 10.7 MB (37.61%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,23 +1,57 @@
 class Solution {
 public:
-    vector<int> sortByBits(vector<int>& arr) {
+    
+    static bool cmp(pair<int, int>&a, pair<int, int>&b){
+        if(a.second<b.second) return true;
+        if(a.second>b.second) return false;
         
-        vector<int> res;
-        //typedef pair<int, int> pi;
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        
-        for(int i=0; i<arr.size(); i++)
-        {
-            int c= __builtin_popcount(arr[i]);
-            pq.push({c, arr[i]});
+        if(a.second==b.second){
+            if(a.first<b.first) return true;
+            
         }
-        
-        while(!pq.empty())
-        {
-            int x= pq.top().second;
-            res.push_back(x);
-            pq.pop();
+        return false;
+    }
+    
+    int Cbits(int n){
+        int res=0;
+        while(n>0){
+            n=n&(n-1);
+            res++;
         }
         return res;
+    }
+    
+    vector<int> sortByBits(vector<int>& arr) {
+        
+//         vector<int> res;
+//         priority_queue<pair<int, int>, vector<pair<int, int>>, 
+//         greater<pair<int, int>>> pq;
+        
+//         for(int i=0; i<arr.size(); i++)
+//         {
+//             int c= __builtin_popcount(arr[i]);
+//             pq.push({c, arr[i]});
+//         }
+        
+//         while(!pq.empty())
+//         {
+//             int x= pq.top().second;
+//             res.push_back(x);
+//             pq.pop();
+//         }
+//         return res;
+        
+        vector<pair<int, int>>res;
+        for(int i=0; i<arr.size(); i++){
+            res.push_back({arr[i], Cbits(arr[i])});
+        }
+        
+        sort(res.begin(), res.end(), cmp);
+        arr.clear();
+        for(auto x:res){
+            arr.push_back(x.first);
+        }
+        
+        return arr;
     }
 };
