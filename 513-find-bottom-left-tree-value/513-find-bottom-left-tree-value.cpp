bd59069734e94d4dd2d85146af5c2a37233b0221
Sumit Anand <60431204+Sumit4399@users.noreply.github.com>
2022-06-09 00:35:41 +0530

    Time: 16 ms (60.29%), Space: 21.8 MB (61.21%) - LeetHub

--- 513-find-bottom-left-tree-value/513-find-bottom-left-tree-value.cpp
+++ 513-find-bottom-left-tree-value/513-find-bottom-left-tree-value.cpp
@@ -14,27 +14,58 @@ public:
     int findBottomLeftValue(TreeNode* root) {
         
         //level order traversal
-        int res = 0, i;
+        if(root==NULL) return -1;
+        if(root->left==NULL && root->right==NULL) return root->val;
+        int res=(root->left!=NULL)?root->left->val:root->right->val;
+        
         queue<TreeNode*> q;
         q.push(root);
-        while (q.size())
+        bool flag=false;
+        
+        while (!q.empty())
         {
-            for (i = q.size() - 1, res = 0; i >= 0; --i) 
+            for (int i = q.size() - 1, flag=false; i >= 0; --i) 
             {
-                TreeNode* node = q.front(); q.pop();
-                res = node->val;
+                TreeNode* curr = q.front(); q.pop();
                 
-                if(node->left==NULL && node->right==NULL)
-                {
-                    q.empty();
-                    break;
+                if(curr->left!=NULL && flag==false){
+                    flag=true;
+                    res=curr->left->val;
+                    q.push(curr->left);
+                }
+                else if(curr->left!=NULL){
+                    q.push(curr->left);
                 }
                 
-                if (node->right) q.push(node->right);
-                if (node->left)  q.push(node->left);
-                
+                if(curr->left==NULL && curr->right!=NULL && flag==false){
+                    flag=true;
+                    res=curr->right->val;
+                    q.push(curr->right);
+                }
+                else if(curr->right!=NULL){
+                    q.push(curr->right);
+                }
             }
         }
         return res;
+        
+//         while (q.size())
+//         {
+//             for (i = q.size() - 1, res = 0; i >= 0; --i) 
+//             {
+//                 TreeNode* node = q.front(); q.pop();
+//                 res = node->val;
+                
+//                 if(node->left==NULL && node->right==NULL)
+//                 {
+//                     q.empty();
+//                     break;
+//                 }
+//                 if (node->right) q.push(node->right);
+//                 if (node->left)  q.push(node->left);
+                
+//             }
+//         }
+//         return res;
     }
 };
