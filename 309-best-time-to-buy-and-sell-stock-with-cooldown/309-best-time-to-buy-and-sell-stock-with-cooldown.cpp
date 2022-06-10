da6df93e0e80bb24acd9688b0ec9070b0f57c3bb
Sumit Anand <60431204+Sumit4399@users.noreply.github.com>
2022-06-10 13:02:11 +0530

    Time: 0 ms (100.00%), Space: 12 MB (21.78%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,16 +1,32 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        
-         int sellstate=0,cooldownstate=0,holdstate=0-prices[0],temp,temp1;
-        for(int i=0;i<prices.size();i++)
+    
+        int memo(int i, int buy, vector<int> &val, int n, vector<vector<int>> &dp)
+    {
+        int profit=0;
+        //base case
+        if(i>=n)
+            return 0;
+        if(dp[i][buy] != -1)
+            return dp[i][buy];
+
+        if(buy==1)
         {
-            temp=holdstate;
-            temp1=sellstate;
-            holdstate=max(holdstate,cooldownstate-prices[i]);
-            sellstate=max(sellstate,temp+prices[i]);
-            cooldownstate =max(cooldownstate,temp1);
+            profit= max(-val[i] + memo(i+1, 0, val, n, dp), //bought
+                        0+ memo(i+1, 1, val, n, dp));       //not bought
         }
-        return sellstate;
+        else
+        {
+            profit= max(val[i] + memo(i+2, 1, val, n, dp), //sold
+                         0 + memo(i+1, 0, val, n, dp));    //not sold
+        }
+        return dp[i][buy]= profit;
+    }
+ 
+    int maxProfit(vector<int>& val) {
+        
+        int n= val.size();
+        vector<vector<int>> dp(n+1, vector<int> (2,-1));
+        return memo(0, 1, val, n, dp);
     }
 };
