357b4854c1fa580d77ba99cc5c119a785bbb04fa
Sumit Anand <60431204+Sumit4399@users.noreply.github.com>
2022-06-12 12:21:41 +0530

    Time: 20 ms (68.67%), Space: 10.5 MB (65.62%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,25 +1,50 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
+    
+    //Using dp--> O(N*N)
+//     int lengthOfLIS(vector<int>& nums) {
         
-        int n=nums.size();
-        if(n==0)
-            return 0;
+//         int n=nums.size();
+//         if(n==0)
+//             return 0;
         
-        vector<int> dp(n,1);
-        int res=1;
+//         vector<int> dp(n,1);
+//         int res=1;
+        
+//         for(int i=1; i<n; i++)
+//         {
+//             for(int j=0; j<i; j++)
+//             {
+//                 if(nums[i]>nums[j])
+//                 {
+//                     dp[i]= max(dp[i], dp[j]+1);
+//                 }
+//             }
+//             res= max(res, dp[i]);
+//         }
+//         return res;
+//     }
+    
+    
+    //Using binary search-->O(NLOGN)
+    int lengthOfLIS(vector<int>& nums){
+        
+        int n= nums.size();
+        vector<int> temp;
+        temp.push_back(nums[0]);
         
         for(int i=1; i<n; i++)
         {
-            for(int j=0; j<i; j++)
+            if(nums[i] > temp.back())
             {
-                if(nums[i]>nums[j])
-                {
-                    dp[i]= max(dp[i], dp[j]+1);
-                }
+                temp.push_back(nums[i]);
             }
-            res= max(res, dp[i]);
+            else
+            {
+                int idx= lower_bound(temp.begin(), temp.end(), nums[i])- temp.begin();
+                temp[idx]= nums[i];
+            }
         }
-        return res;
+        return temp.size();
     }
 };
