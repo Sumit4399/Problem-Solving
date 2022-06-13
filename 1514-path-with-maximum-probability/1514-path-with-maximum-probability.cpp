e5ef74afc9f1f6b5ed77bd9cf7ec1ee7aa1eb1e4
Sumit Anand <60431204+Sumit4399@users.noreply.github.com>
2022-06-13 19:43:29 +0530

    Time: 240 ms (69.01%), Space: 70.8 MB (17.95%) - LeetHub

---
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& Prob, int start, int end) {
+        
+        vector<bool> vis(n, false);
+        priority_queue<pair<double, int>> pq;
+        pq.push({(double)1.0, start});
+        
+        vector<vector<pair<int, double>>> graph(n);
+        for(int i = 0; i < edges.size(); i++)
+        {
+            vector<int> edge = edges[i];
+            graph[edge[0]].push_back({edge[1], Prob[i]});
+            graph[edge[1]].push_back({edge[0], Prob[i]});
+        }
+    
+        while(!pq.empty())
+        {
+            auto[pb, pos]= pq.top();
+            pq.pop();
+            
+            if(pos==end)
+                return pb;
+            
+            if(vis[pos]==true)
+                continue;
+            vis[pos]=true;
+            
+            for(auto it : graph[pos])
+            {
+                if(vis[it.first] != true)
+                    pq.push({pb*it.second, it.first});
+            }
+        }
+        return 0;
+    }
+};
