e06099af46712ee8905e176ec3c2ab5f44db758e
Sumit Anand <60431204+Sumit4399@users.noreply.github.com>
2022-06-22 12:17:39 +0530

    Time: 460 ms (43.58%), Space: 88 MB (40.64%) - LeetHub

--- 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.cpp
+++ 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     
-    struct comparator{
+//     struct comparator{
         
-    bool operator()(string &s1, string&s2){
-        if(s1.length()>s2.length())
-            return false;
-        else if(s1.length()<s2.length())
-            return true;
+//     bool operator()(string &s1, string&s2){
+//         if(s1.length()>s2.length())
+//             return false;
+//         else if(s1.length()<s2.length())
+//             return true;
         
-        else{
+//         else{
             
-            for(int i=0; i<s1.size(); i++){
-                if(s1[i]-'0'>s2[i]-'0')
-                    return false;
-                else if(s1[i]-'0'<s2[i]-'0')
-                    return true;
-            }
-        }
-        return true;
-    }
-};
+//             for(int i=0; i<s1.size(); i++){
+//                 if(s1[i]-'0'>s2[i]-'0')
+//                     return false;
+//                 else if(s1[i]-'0'<s2[i]-'0')
+//                     return true;
+//             }
+//         }
+//         return true;
+//     }
+// };
     
-    string kthLargestNumber(vector<string>& nums, int k) {
+ //   string kthLargestNumber(vector<string>& nums, int k) {
         
 //         priority_queue<int> pq;
 //         vector<int> v;
@@ -48,17 +48,62 @@ public:
 //         return ans;
         
         
+//         priority_queue<string, vector<string>, comparator>pq;
+//         for(auto it:nums)
+//         {
+//             pq.push(it);
+//         }
+        
+//         while(k-1>0)
+//         {
+//             pq.pop();
+//             k--;
+//         }
+//         return pq.top();
+//     }
+    
+    
+    struct comparator{
+        
+    bool operator()(string &s1, string&s2){
+        if(s1.length()<s2.length())
+            return true;
+        else if(s1.length()>s2.length())
+            return false;
+        
+        else{
+            
+            for(int i=0; i<s1.size(); i++){
+                if(s1[i]-'0'<s2[i]-'0')
+                    return true;
+                else if(s1[i]-'0'>s2[i]-'0')
+                    return false;
+            }
+        }
+        
+        return false;
+    }
+};
+    
+    string kthLargestNumber(vector<string>& nums, int k) {
+        
+        //min heap
         priority_queue<string, vector<string>, comparator>pq;
+        string ans;
+        
         for(auto it:nums)
         {
             pq.push(it);
         }
         
-        while(k-1>0)
+        while(!pq.empty() && k>0)
         {
+            ans=pq.top();
             pq.pop();
             k--;
         }
-        return pq.top();
+        
+        return ans;
+        
     }
 };
