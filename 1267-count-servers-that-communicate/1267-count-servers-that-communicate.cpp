aa6900b47d768e1b84e39bd6b80a8b004e9e7a8b
Sumit Anand <60431204+Sumit4399@users.noreply.github.com>
2022-06-23 23:47:35 +0530

    Time: 114 ms (28.87%), Space: 22.4 MB (30.61%) - LeetHub

--- 1267-count-servers-that-communicate/1267-count-servers-that-communicate.cpp
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.cpp
@@ -1,60 +1,80 @@
 class Solution {
 public:
     
-//     void dfs(int i, int j, int r, int c, vector<vector<int>>& grid, int &temp){
-//         if(i<0 || j<0 || j>=c || i>=r || grid[i][j]==0)
-//             return;
-        
-//         temp++;
-//         grid[i][j]=0;
-        
-//         //calling dfs in all 4 directions
-//         dfs(i-1, j, r, c, grid, temp);
-//         dfs(i, j-1, r, c, grid, temp);
-//         dfs(i+1, j, r, c, grid, temp);
-//         dfs(i, j+1, r, c, grid, temp);
-        
-//     }
-    
     int countServers(vector<vector<int>>& grid) {
-        int res=0;
-        int r= grid.size();
-        int c= grid[0].size();
-        bool temp= false;
         
-        for(int i=0; i<r; i++)
+        //brute force (O(N*N*N))
+//         int res=0;
+//         int r= grid.size();
+//         int c= grid[0].size();
+//         bool temp= false;
+        
+//         for(int i=0; i<r; i++)
+//         {
+//             for(int j=0; j<c; j++)
+//             {
+//                 temp=false;
+//                 if(grid[i][j]==1)
+//                 {
+//                     for(int k=0; k<c; k++)
+//                     {
+//                         if(grid[i][k] == 1 && k!=j)
+//                         {
+//                             temp= true;
+//                             continue;
+//                             //break;
+//                         }
+//                     }
+//                     for(int k=0; k<r; k++)
+//                     {
+//                         if(grid[k][j] == 1 && k!=i)
+//                         {
+//                             temp= true;
+//                         }
+//                     }
+//                     if(temp==true)
+//                     {
+//                         res++;
+//                     }
+//                 }
+//             }
+//         }
+//         return res;
+        
+        
+        
+    //OPTIMSED (O(N*N))
+        int n = grid.size();
+        int m = grid[0].size();
+        int count = 0;
+        
+        vector<int> row_count(n, 0);
+        vector<int> col_count(m, 0);
+        
+        for(int i = 0; i < n; i++)
         {
-            for(int j=0; j<c; j++)
+            for(int j = 0; j < m; j++)
             {
-                temp=false;
-                if(grid[i][j]==1)
+                if(grid[i][j])
                 {
-                    for(int k=0; k<c; k++)
-                    {
-                        if(grid[i][k] == 1 && k!=j)
-                        {
-                            temp= true;
-                            //continue;
-                            break;
-                        }
-                    }
-                    
-                    for(int k=0; k<r; k++)
-                    {
-                        if(grid[k][j] == 1 && k!=i)
-                        {
-                            temp= true;
-                        }
-                    }
-                    
-                    if(temp==true)
-                    {
-                        res++;
-                    }
+                    row_count[i]++;
+                    col_count[j]++;
                 }
             }
         }
         
-        return res;
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < m; j++)
+            {
+                if(grid[i][j] && (row_count[i] >= 2 || col_count[j] >= 2 ))
+                {
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+
     }
 };
