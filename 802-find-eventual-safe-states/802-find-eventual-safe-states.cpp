7f5c8d5850775f2e481f304f76f31053a331fcc6
Sumit Anand <60431204+Sumit4399@users.noreply.github.com>
2022-07-02 20:58:08 +0530

    Time: 206 ms (71.22%), Space: 47.1 MB (77.67%) - LeetHub

--- 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
@@ -1,76 +1,30 @@
 class Solution {
 public:
     
- //   vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+    bool dfs(vector<vector<int>>& graph, vector<int> &color, int node){
         
-//         vector<int> res;
-//         unordered_set<int> set;
-//         for(int i=0; i<graph.size(); i++)
-//         {
-//             if(graph[i].size()==0)
-//                 set.insert(i);
-//         }
+        color[node] = 1;
         
-//         for(int i=0; i<graph.size(); i++)
-//         {
-//             bool isRes = true;
-//             for(int j=0;j<graph[i].size();j++) {
-//                 if(set.count(graph[i][j])) {
-//                     continue;
-//                 }
-//                 else {
-//                     isRes = false;
-//                     break;
-//                 }
-//             }
-//             if(isRes) {
-//                 res.push_back(i);
-//             }
-//         }
-//         return res;
-        
-        
-        
-         bool DFSRec(int s, vector<bool>&visited, vector<bool>&dfsVisited, vector<vector<int>>& graph, vector<bool> &present_cycle){
-        visited[s] = true;
-        dfsVisited[s] = true;
-        
-        vector<int>data = graph[s];
-        for(auto x: data){
-            if(!visited[x]){
-                if(DFSRec(x, visited, dfsVisited, graph, present_cycle)){
-                    return present_cycle[s] = true;
-                }
-            }
-            else if(visited[x] && dfsVisited[x]){
-                return present_cycle[s] = true;
-            }
+        for(auto curr : graph[node])
+        {
+            if((color[curr]==0 && dfs(graph, color, curr)) || color[curr]==1)
+                return true;
         }
         
-        dfsVisited[s] = false;
+        color[node] = 2;
         return false;
     }
     
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        vector<int> ans;
         
-        int n = graph.size();
-        vector<bool>visited(n, false), dfsVisited(n, false);
-        vector<bool> present_cycle(n, false);
+        int n= graph.size();
+        vector<int> color(n), res;
         
-        for(int i=0; i<n; i++){
-            if(!visited[i]){
-                DFSRec(i, visited, dfsVisited,graph, present_cycle);
-            }
+        for(int i=0; i<n; i++)
+        {
+            if(color[i]==2 || !dfs(graph, color, i))
+                res.push_back(i);
         }
-        
-        for(int i=0; i<n; i++){
-            if(!present_cycle[i]){
-                ans.push_back(i);
-            }
-        }
-        
-        return ans;
+        return res;
     }
-    
 };
