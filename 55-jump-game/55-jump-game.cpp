bcb63f938fae414012f96296e4bb6bc358a47f39
Sumit Anand <60431204+Sumit4399@users.noreply.github.com>
2022-07-05 01:07:45 +0530

    Time: 100 ms (55.20%), Space: 48.4 MB (42.95%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        
+        int n= nums.size();
+        int far=0;
+        
+        for(int i=0; i<n; i++)
+        {
+            if(far<i)
+                return false;
+            
+            far= max(i+ nums[i], far);
+        }
+        return true;
+    }
+};
