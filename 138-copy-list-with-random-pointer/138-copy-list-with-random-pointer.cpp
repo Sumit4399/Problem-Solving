a2c2347dacc7408eecb98ae2abdc235d0323a008
Sumit Anand <60431204+Sumit4399@users.noreply.github.com>
2022-07-05 16:14:28 +0530

    Time: 16 ms (38.00%), Space: 11.2 MB (59.86%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -18,85 +18,25 @@ class Solution {
 public:
     Node* copyRandomList(Node* head) {
         
-//         //part-1- inserting new nodes in between given linked list
-//         Node* curr= head;
-//         while(curr)
-//         {
-//             Node* temp =curr->next;
-//             curr->next= new Node(curr->val);
-//             curr->next->next= temp;
-//             curr= temp;
-//         }
-//         curr= head;
+        Node* curr= head;
+        unordered_map<Node*, Node*> mp;
         
-//         //part-2- setting random pointers of new nodes
-//         while(curr)
-//         {
-//             if(curr->next)
-//             {
-//                 curr->next->random= (curr->random) ? curr->random->next: NULL;
-//             }
-//             curr= curr->next->next;
-//         }
-        
-//         //part-3-separating orig and copy linked list
-//         Node* orig= head;
-//         Node* copy= head->next;
-//         Node* temp= copy;
-        
-//         while(orig)
-//         {
-//             orig->next= orig->next->next;
-//             copy->next= copy->next->next;
-//             orig= orig->next;
-//             copy= copy->next;
-//         }
-        
-//         return temp;
-        
-        if(!head) return head;
-        
-        Node* temp=head;
-        
-        //creates mapping of cloned list using original list
-        while(temp)
+        //making a map with ({original ll addrrss node, new ll address node})
+        while(curr!= NULL)
         {
-            Node* newNode= new Node(temp->val);
-            newNode->next=temp->next;
-            temp->next=newNode;
-            temp=newNode->next;
-            
+            mp[curr] = new Node(curr->val);
+            curr= curr->next;
         }
         
-        //assigning random pointers
-        temp=head;
-        while(temp)
+        curr= head;
+        
+        while(curr!= NULL)
         {
-            if(temp->random)
-            {
-               temp->next->random= temp->random->next;   
-            }
-            temp=temp->next->next;
+            mp[curr]->next = mp[curr->next];
+            mp[curr]->random= mp[curr->random];
+            curr= curr->next;
         }
         
-        //restore the original linked list
-        Node* newHead= head->next;
-        Node* temp2=head->next;
-        temp=head;
-        
-        while(temp  && temp2)
-        {
-            Node* currN=temp2->next;
-            temp->next=currN;
-            if(currN)
-            {
-                temp2->next=currN->next;
-            }
-            temp=temp->next;
-            temp2=temp2->next;
-        }
-        
-        return newHead;
- 
+        return mp[head];
     }
 };
