0cf6af39b81d21dc4a9495dfadf731b1b848168c
Sumit Anand <60431204+Sumit4399@users.noreply.github.com>
2022-07-06 11:37:32 +0530

    Time: 26 ms (80.01%), Space: 11.2 MB (50.41%) - LeetHub

--- 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.cpp
+++ 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.cpp
@@ -21,22 +21,42 @@ public:
 class Solution {
 public:
     
-    void pre(Node* root, vector<int> &res){
+    //recursive
+//     void pre(Node* root, vector<int> &res){
         
-        if(root==NULL)
-            return;
+//         if(root==NULL)
+//             return;
         
-        res.push_back(root->val);
+//         res.push_back(root->val);
         
-        for(int i=0; i<root->children.size(); i++)
-            pre(root->children[i], res);
+//         for(int i=0; i<root->children.size(); i++)
+//             pre(root->children[i], res);
         
-    }
+//     }
     
+    
+    //iterative
     vector<int> preorder(Node* root) {
         
         vector<int> res;
-        pre(root, res);
+        if(root== NULL)
+            return res;
+        
+        stack<Node*> st;
+        st.push(root);
+        
+        while(!st.empty())
+        {
+            Node* curr= st.top();
+            st.pop();
+            
+            res.push_back(curr->val);
+            
+            for(int i= curr->children.size()-1; i>=0; i--)
+            {
+                st.push(curr->children[i]);
+            }
+        }
         return res;
     }
 };
