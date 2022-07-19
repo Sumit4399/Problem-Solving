d0f6d9db0b5baea6ec0ee30fb59b30f9de28b9d1
Sumit Anand <60431204+Sumit4399@users.noreply.github.com>
2022-07-19 10:31:28 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (31.97%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
+    
     vector<vector<int>> generate(int num) {
         
-    //dp approach
-    vector<vector<int>> res;
-	for (int i = 0; i < num; i++) {
-		vector<int> row(i + 1, 1);
-		for (int j = 1; j < i; j++) {
-			row[j] = res[i - 1][j] + res[i - 1][j - 1];
-		}
-		res.push_back(row);
-	}
-	return res;
+        //dp approach
+        vector<vector<int>> res(num);
+        for (int i = 0; i < num; i++)
+        {
+            res[i]= vector<int>(i+1, 1);
+            for (int j = 1; j < i; j++)
+            {
+                res[i][j] = res[i - 1][j] + res[i - 1][j - 1];
+            }
+        }
+        return res;
     }
 };
