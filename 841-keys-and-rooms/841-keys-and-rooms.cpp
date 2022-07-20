7a7fc3ad8269835e971d604b64f002b320d4b50c
Sumit Anand <60431204+Sumit4399@users.noreply.github.com>
2022-07-20 20:39:54 +0530

    Time: 24 ms (15.53%), Space: 10.4 MB (55.51%) - LeetHub

--- 841-keys-and-rooms/841-keys-and-rooms.cpp
+++ 841-keys-and-rooms/841-keys-and-rooms.cpp
@@ -1,24 +1,53 @@
 class Solution {
 public:
+//     bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        
+//         int n=rooms.size();
+//         vector<bool> vis(n, false);
+//         queue<int> q;
+//         q.push(0);
+        
+//         while(!q.empty())
+//         {
+//             int curr_room= q.front();
+//             vis[curr_room]=true;
+//             q.pop();
+            
+//             for(auto it : rooms[curr_room])
+//             {
+//                 if(vis[it]==false)
+//                     q.push(it);
+//             }
+//         }
+        
+//         for(auto it: vis)
+//         {
+//             if(it==false)
+//                 return false;
+//         }
+//         return true;
+//     }
+    
+    
+    //dfs approach
+    void dfs(vector<vector<int>>& rooms, vector<bool> &vis, int i){
+        
+        vis[i]=true;
+        for(auto it : rooms[i])
+        {
+            if(!vis[it])
+            dfs(rooms, vis, it);
+        }
+    }
+    
+    
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         
         int n=rooms.size();
         vector<bool> vis(n, false);
-        queue<int> q;
-        q.push(0);
         
-        while(!q.empty())
-        {
-            int curr_room= q.front();
-            vis[curr_room]=true;
-            q.pop();
-            
-            for(auto it : rooms[curr_room])
-            {
-                if(vis[it]==false)
-                    q.push(it);
-            }
-        }
+        //calling the function
+        dfs(rooms, vis, 0);
         
         for(auto it: vis)
         {
