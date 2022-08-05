5e2207c3f388dca553019010857bcbf30cd9b9c0
Sumit Anand <60431204+Sumit4399@users.noreply.github.com>
2022-08-05 12:56:50 +0530

    Time: 3 ms (62.90%), Space: 6.7 MB (65.95%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,33 +1,46 @@
 class Solution {
 public:
     
-    int memo(int tar, vector<int>& nums, vector<int> &dp){
+    //memoization
+//     int memo(int tar, vector<int>& nums, vector<int> &dp){
         
-      //base case
-      if(tar==0)
-          return 1;
-      if(dp[tar] != -1)
-          return dp[tar];
+//       //base case
+//       if(tar==0)
+//           return 1;
+//       if(dp[tar] != -1)
+//           return dp[tar];
     
-        //recursive code
-        dp[tar]=0;
-        
-        for(int i=0; i<nums.size(); i++)
-        {
-            if(nums[i] <= tar)
-            {
-                dp[tar]+= memo(tar-nums[i], nums, dp);
-            }
-        }
-        
-        return dp[tar];
-    }
+//         //recursive code
+//         dp[tar]=0;
+//         for(int i=0; i<nums.size(); i++)
+//         {
+//             if(nums[i] <= tar)
+//             {
+//                 dp[tar]+= memo(tar-nums[i], nums, dp);
+//             }
+//         }
+//         return dp[tar];
+//     }
     
+    
+    //tabulation
     int combinationSum4(vector<int>& nums, int tar) {
         
         int n=nums.size();
-        vector<int> dp(tar+1, -1);
+        vector<uint> dp(tar+1, 0);
         
-        return memo(tar, nums, dp);
+        dp[0]=1;
+        
+        for(int curr=0; curr<=tar; curr++)
+        {
+            for(int i=0; i<nums.size(); i++)
+            {
+                if(nums[i] <= curr)
+                {
+                    dp[curr]+= dp[curr-nums[i]];
+                }
+            }
+        }
+        return dp[tar];
     }
 };
