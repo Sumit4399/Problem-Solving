4ed9b084727603fa057bb1a0680999f9b519d56c
Sumit Anand <60431204+Sumit4399@users.noreply.github.com>
2022-09-23 18:29:12 +0530

    Time: 39 ms (45.34%), Space: 12.6 MB (5.37%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -3,25 +3,22 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         
         vector<int>v;
-        
         map<int, int>mp;
         
-        for(int i=0; i<nums.size(); i++){
-            if(mp.find(target-nums[i])!=mp.end()){
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(mp.find(target-nums[i])!=mp.end())
+            {
                 v.push_back(i);
                 v.push_back(mp[target-nums[i]]);
                 
-                return v;
+                //return v;
             }
             else
             {
                 mp.insert({nums[i], i});
             }
         }
-        
-        return v;
-        
-        
-        
+        return v; 
     }
 };
