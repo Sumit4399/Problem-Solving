943d8f42a663fbcec53073d82b1296d58cf87bcc
Sumit Anand <60431204+Sumit4399@users.noreply.github.com>
2023-04-10 17:28:20 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (56.25%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        
+        if(s.size()==0)
+            return false;
+        
+        stack<char> st;
+        for(char &ch: s)
+        {
+            if(ch=='(' || ch=='{' || ch=='[')
+                st.push(ch);
+            else
+            {
+                if(st.empty())
+                    return false;
+                
+                if(st.top()=='(' && ch==')')
+                    st.pop();
+                else if(st.top()=='{' && ch=='}')
+                    st.pop();
+                else if(st.top()=='[' && ch==']')
+                    st.pop();
+                else
+                    return false;
+            }
+        }
+        
+        if(!st.empty())
+            return false;
+        else
+            return true;
+    }
+};
